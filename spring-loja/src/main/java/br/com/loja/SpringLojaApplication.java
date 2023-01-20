fee58f7bfb33fc84ab6db4ddd34d9de6a2d59388
israelaze <israelaze@gmail.com>
2023-01-20 19:24:30 -0300

    Iniciando Projeto SpringBoot

---
+++ spring-loja/src/main/java/br/com/loja/SpringLojaApplication.java
@@ -0,0 +1,13 @@
+package br.com.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLojaApplication.class, args);
+	}
+
+}
---
+++ spring-loja/src/test/java/br/com/loja/SpringLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
