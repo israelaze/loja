634064dbed203390e85f697512e72a8aca5f9213
israelaze <israelaze@gmail.com>
2023-01-20 20:20:11 -0300

    Criando entidade Vendedor, VendedorRepository e Perfil de teste

---
+++ spring-loja/src/main/java/br/com/loja/entities/Vendedor.java
@@ -0,0 +1,39 @@
+package br.com.loja.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "vendedores")
+public class Vendedor implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Setter(AccessLevel.NONE)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idVendedor;
+
+	@Column(length = 100, nullable = false)
+	private String nome;
+
+	@Column(length = 50)
+	private String apelido;
+	
+	//@OneToMany(mappedBy = "vendedor")
+	//private Set<Pedido> pedidos = new HashSet<>();
+
+}
