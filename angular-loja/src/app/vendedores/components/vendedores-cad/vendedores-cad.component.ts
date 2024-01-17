bb860a06b6f4520ba72f12c4cf4b746196506e55
israelaze <israelaze@gmail.com>
2024-01-17 13:32:46 -0300

    Cadastrando novo Vendedor

--- angular-loja/src/app/vendedores/components/vendedores-cad/vendedores-cad.component.ts
+++ angular-loja/src/app/vendedores/components/vendedores-cad/vendedores-cad.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/util/services/alert.service';
+import { Vendedor } from '../../models/vendedor';
+import { VendedoresService } from './../../services/vendedores.service';
 
 @Component({
   selector: 'app-vendedores-cad',
@@ -6,5 +11,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./vendedores-cad.component.scss']
 })
 export class VendedoresCadComponent {
+
+  constructor(private formBuilder: FormBuilder,
+    private vendedoresService: VendedoresService,
+    private alertService: AlertService,
+    private router: Router){}
+
+
+
+  form = this.formBuilder.group({
+    nome: ['', Validators.required],
+    apelido: ['']
+  });
+
+  cadastrar(){
+
+    const vendedor: Vendedor = this.populaDadosForm();
+    console.log();
+
+
+    this.vendedoresService.cadastrar(vendedor).subscribe({
+      next: result => {
+        this.onSuccess(result);
+      },
+      error: e => {
+        this.onError(e);
+      }
+    });
+  }
+
+  private onSuccess(result: Vendedor) {
+    this.alertService.success('Vendedor ' + result.nome+ ' cadastrado(a) com sucesso');
+    this.router.navigate(['vendedores/vendedores-lista/']);
+  }
+
+  private onError(e: any) {
+
+
+    console.log(e.error.errors[0].message);
+
+    this.alertService.error(e.error.message + ': ' + e.error.errors[0].message);
+  }
+
+  populaDadosForm(): Vendedor{
+    return {
+      nome: this.form.value.nome,
+      apelido: this.form.value.apelido
+    };
+  }
+
+  getErrorMessage(fieldName: string){
+
+    const form = this.form.get(fieldName);
+
+    if(form?.hasError('required')){
+      return 'Campo obrigatório'
+    }
+
+    return 'Inválido';
+  }
 
 }
--- spring-loja/src/main/java/br/com/loja/dtos/vendedor/VendedorPostDTO.java
+++ spring-loja/src/main/java/br/com/loja/dtos/vendedor/VendedorPostDTO.java
@@ -1,11 +1,13 @@
 package br.com.loja.dtos.vendedor;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class VendedorPostDTO {
 	
+	@Size(min= 3, max = 50, message = "{nome.size}")
 	@NotBlank(message = "{nome.not.blank}")
 	private String nome;
 	
