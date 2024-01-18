3ba1dcadf057058b07d39f6ef4306adef9908f37
israelaze <israelaze@gmail.com>
2024-01-18 13:12:30 -0300

    Classe configuração do CORS

--- spring-loja/src/main/java/br/com/loja/configs/security/SecurityConfiguration.java
+++ spring-loja/src/main/java/br/com/loja/configs/security/SecurityConfiguration.java
@@ -44,7 +44,6 @@ public class SecurityConfiguration {
 		}
 	  
     http
-    	.cors().and()
         .csrf()
         .disable()
         .authorizeHttpRequests(auth -> auth
@@ -76,19 +75,19 @@ public class SecurityConfiguration {
     return http.build();
   }
   
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-      CorsConfiguration configuration = new CorsConfiguration();
-      configuration.setAllowedOrigins(Arrays.asList("https://loja-gpvx.onrender.com"));
-      configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-      configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-      configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-      configuration.setExposedHeaders(Arrays.asList("Authorization"));
-
-      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-      source.registerCorsConfiguration("/**", configuration);
-      return source;
-  }
+//  @Bean
+//  public CorsConfigurationSource corsConfigurationSource() {
+//      CorsConfiguration configuration = new CorsConfiguration();
+//      configuration.setAllowedOrigins(Arrays.asList("https://loja-gpvx.onrender.com"));
+//      configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+//      configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//      configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+//      configuration.setExposedHeaders(Arrays.asList("Authorization"));
+//
+//      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//      source.registerCorsConfiguration("/**", configuration);
+//      return source;
+//  }
   
 //	@Bean
 //	CorsConfigurationSource corsConfigurationSource() {
